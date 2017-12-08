d8d4df43edb11d828943b05a24eb5b6f76f77aa6
Fadion96 <nowak196@gmail.com>
2017-12-08 15:09:40 +0100

    Added simple game model

--- src/main/common/model/game/Game.java
+++ src/main/common/model/game/Game.java
@@ -11,19 +11,29 @@ public class Game {
 
     private static int nrPlayers;
 
-    private final static int widths[] = {1,1};
+    private final static int widths[] = {1,2,3,4,13,12,11,10,9,10,11,12,13,4,3,2,1};
 
-    private final static int offsets[] = {1,1};
+    private final static int offsets[] = {4,4,4,4,0,1,2,3,4,4,4,4,4,9,10,11,12};
 
-    Game(int size){
+    Game(int nrPlayers,int size){
+        this.board = new Marble[size][size];
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(j < offsets[i] || j > offsets[i] + widths[i] ){
+                    board[i][j] = null;
+                }
+                else{
+                    board[i][j] = new Marble(i, j, Color.NONE, Color.NONE);
+                }
+            }
+        }
+    }
+
+    public void makeMove(int prevX, int prevY, int nextX, int nextY){
 
     }
 
-    public void makemove(int x, int y, int z, int w){
-
+    public Marble[][] getBoard() {
+        return board;
     }
-
-
-
-
 }
--- src/main/common/model/game/Marble.java
+++ src/main/common/model/game/Marble.java
@@ -1,4 +1,50 @@
 package common.model.game;
 
 public class Marble {
+    private int x;
+
+    private int y;
+
+    private Color color;
+
+    private Color territory;
+
+    Marble(int x, int y, Color color, Color territory){
+        this.setX(x);
+        this.setY(y);
+        this.setColor(color);
+        this.setTerritory(territory);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Color getTerritory() {
+        return territory;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setTerritory(Color territory) {
+        this.territory = territory;
+    }
 }
